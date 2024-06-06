d7dcc377fc793e41b9b4bfa360b725bef36d92e7
Mikhail1804-rus <mihaelle3@gmail.com>
2024-06-06 22:48:51 +0700

    fix wrong import paths

--- src/pages/Home/components/Certificates/data.js
+++ src/pages/Home/components/Certificates/data.js
@@ -1,13 +1,13 @@
 import first from "/public/assets/serf1.jpg"
-import second from "/public/assets/serf1.jpg"
-import third from "/public/assets/serf1.jpg"
-import fourth from "/public/assets/serf1.jpg"
-import fifth from "/public/assets/serf1.jpg"
-import sixth from "/public/assets/serf1.jpg"
-import seventh from "/public/assets/serf1.jpg"
-import eights from "/public/assets/serf1.jpg"
-import ninth from "/public/assets/serf1.jpg"
-import tenth from "/public/assets/serf1.jpg"
+import second from "/public/assets/serf2.jpg"
+import third from "/public/assets/serf3.jpg"
+import fourth from "/public/assets/serf4.jpg"
+import fifth from "/public/assets/serf5.png"
+import sixth from "/public/assets/serf6.jpg"
+import seventh from "/public/assets/serf7.jpg"
+import eights from "/public/assets/serf8.jpg"
+import ninth from "/public/assets/serf9.jpg"
+import tenth from "/public/assets/serf10.jpg"
 
 export const certificatesImagesData = [
     first,
