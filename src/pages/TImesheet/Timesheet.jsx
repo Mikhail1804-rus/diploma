1020ac30128fdc89212b64f7ea4c2716d80f7581
Mikhail1804-rus <mihaelle3@gmail.com>
2024-06-10 21:13:53 +0700

    add server logic to timesheet page

--- src/pages/TImesheet/Timesheet.jsx
+++ src/pages/TImesheet/Timesheet.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Timesheet.module.scss'
-import {timesheetData} from "./data.js";
 
 function Timesheet(props) {
-    const timesheet = timesheetData
+    const [timesheet, setTimesheet] = useState([]);
+
+    const url = 'http://localhost:3000/api/timesheet/getList'
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setTimesheet(data))
+            .catch(err => console.error(err));
+    }, []);
 
     return (
         <div className={s.timesheet}>
@@ -18,18 +26,9 @@ function Timesheet(props) {
                     </div>
                     {
                         timesheet.length && timesheet.map((item) => (
-                            <div key={item.name} className={s.table__row}>
-                                <div>{item.name}</div>
-                                <div>
-                                    {item.dates.length && item.dates.map((date, index) => {
-                                        const mappedDate = date
-                                        if (index !== item.dates.length - 1) {
-                                            return date + ', '
-                                        }
-
-                                        return <span key={mappedDate}>{mappedDate}</span>
-                                    })}
-                                </div>
+                            <div key={item.courseName} className={s.table__row}>
+                                <div>{item.courseName}</div>
+                                <div> {item.dates}</div>
                                 <div>{item.time}</div>
                             </div>
                         ))
